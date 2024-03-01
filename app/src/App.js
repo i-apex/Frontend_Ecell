4e35cc9ffabfdb9dcea387282f299f953cd6bf3d
i_apex <jaim7viramgama@gmail.com>
2024-03-01 17:17:46 +0530

    App added

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+function App() {
+  return (
+    <div>
+      <ButtonUsage />
+    </div>
+  );
+}
+
+export default App;
