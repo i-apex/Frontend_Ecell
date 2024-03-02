e78d45aae7648ac4897c9c946edea4ba51025176
cs22b047 <cs22b047@iittp.ac.in>
2024-03-03 00:32:56 +0530

    minor css changes in teams

--- my-app/src/components/Team.js
+++ my-app/src/components/Team.js
@@ -6,16 +6,15 @@ const Team = (props) => {
     return (
         <>
             <div className="team-div">
-                <div>
-                    <h2 style={{ color: "white" }}>{props.team_name}</h2>
-                    <p style={{color: "white"}}>The sun peeked over the horizon, painting the sky in hues of pink and gold. Birds chirped joyously, welcoming the new day with their melodious songs. In the quaint little town of Willow Creek, life began to stir as people woke up to the promise of another beautiful morning. The aroma of freshly brewed coffee wafted through the air, mingling with the scent of blooming flowers. It was a day ripe with possibilities, a blank canvas waiting to be painted with adventures and memories.</p>
+                <div className="team-decription">
+                        <h2 style={{ color: "white" }}>{props.team_name}</h2>
+                        <p style={{ color: "white" }}>The sun peeked over the horizon, painting the sky in hues of pink and gold. Birds chirped joyously, welcoming the new day with their melodious songs. In the quaint little town of Willow Creek, life began to stir as people woke up to the promise of another beautiful morning. The aroma of freshly brewed coffee wafted through the air, mingling with the scent of blooming flowers. It was a day ripe with possibilities, a blank canvas waiting to be painted with adventures and memories.</p>
                 </div>
-                {props.team_members.map((members) => {
-                    if (members.Position == "Lead") {
+                <div className="team-members">
+                    {props.team_members.map((members) => {
                         return <Card1 name={members.Name} position={members.Position}></Card1>
-                    } else
-                        return <Card1 name={members.Name} position={members.Position}></Card1>
-                })}
+                    })}
+                </div>
             </div>
         </>
     )
